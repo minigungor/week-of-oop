3e311224255fd53ddec34980f06471247341966d
minigungor <siski_02@internet.ru>
2026-02-06 07:04:42 +0000

    lesson06-finished

---
+++ lesson06/order/demo01.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace lesson06\order\demo01;
+
+use yii\db\ActiveRecord;
+use Yii;
+
+class Order extends ActiveRecord
+{
+    const STATUS_NEW = 0;
+    const STATUS_PAID = 1;
+    const STATUS_SENT = 2;
+}
+
+$order = $this->findModel(5);
+
+########
+
+$order->status = Order::STATUS_PAID;
+$order->save();
+
+$order->updateAttributes(['status' => Order::STATUS_PAID]);
+
+Order::updateAll(['status' => Order::STATUS_SENT], ['id' => $order->id]);
+
+Yii::$app->db->createCommand()
+    ->update(Order::tableName(), ['status' => Order::STATUS_PAID], ['id' => $order->id])
+    ->execute();
